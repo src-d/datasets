6f195a7064042f2f1c6a2bc2cffbe650d0b8f70c
Francesc Campoy <campoy@golang.org>
2018-03-02 17:16:45 -0800

    simplified downloading process
    Signed-off-by: Francesc Campoy <campoy@golang.org>

--- PublicGitArchive/pga/cmd/get.go
+++ PublicGitArchive/pga/cmd/get.go
@@ -1,18 +1,15 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/src-d/datasets/PublicGitArchive/pga/pga"
-	"golang.org/x/sync/errgroup"
 	pb "gopkg.in/cheggaaa/pb.v1"
 )
 
@@ -24,6 +21,8 @@ var getCmd = &cobra.Command{
 
 Alternatively, a list of .siva filenames can be passed through standard input.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		source := urlFS("http://pga.sourced.tech/")
+
 		dest, err := FileSystemFromFlags(cmd.Flags())
 		if err != nil {
 			return err
@@ -34,7 +33,7 @@ Alternatively, a list of .siva filenames can be passed through standard input.`,
 			return err
 		}
 
-		var index pga.Index
+		var filenames []string
 
 		stdin, err := cmd.Flags().GetBool("stdin")
 		if err != nil {
@@ -43,7 +42,11 @@ Alternatively, a list of .siva filenames can be passed through standard input.`,
 		if stdin {
 			fmt.Fprintln(os.Stderr, "downloading siva files by name from stdin")
 			fmt.Fprintln(os.Stderr, "filter flags will be ignored")
-			index = getIndexFromStdin(os.Stdin)
+			b, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				return fmt.Errorf("could not read from standard input: %v", err)
+			}
+			filenames = strings.Split(string(b), "\n")
 		} else {
 			f, err := getIndex()
 			if err != nil {
@@ -51,7 +54,7 @@ Alternatively, a list of .siva filenames can be passed through standard input.`,
 			}
 			defer f.Close()
 
-			index, err = pga.IndexFromCSV(f)
+			index, err := pga.IndexFromCSV(f)
 			if err != nil {
 				return err
 			}
@@ -61,93 +64,52 @@ Alternatively, a list of .siva filenames can be passed through standard input.`,
 				return err
 			}
 			index = pga.WithFilter(index, filter)
-		}
 
-		tokens := make(chan bool, maxDownloads)
-		for i := 0; i < maxDownloads; i++ {
-			tokens <- true
-		}
-
-		var jobsDone int
-		var mutex sync.Mutex
-		withMutex := func(f func()) { mutex.Lock(); f(); mutex.Unlock() }
-		var totalJobs int
-
-		var group errgroup.Group
-		for {
-			r, err := index.Next()
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				return err
-			}
-
-			totalJobs += len(r.Filenames)
-
-			for _, filename := range r.Filenames {
-				filename := filename // avoid data race
-				group.Go(func() error {
-					<-tokens
-					defer func() {
-						withMutex(func() { jobsDone++ })
-						tokens <- true
-					}()
-					if err := get(dest, filename); err != nil {
-						fmt.Fprintf(os.Stderr, "could not get %s: %v\n", filename, err)
-					}
-					return nil
-				})
+			for {
+				r, err := index.Next()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					return err
+				}
+				filenames = append(filenames, r.Filenames...)
 			}
 		}
 
-		done := make(chan struct{})
-		go func() { group.Wait(); close(done) }()
-
-		bar := pb.StartNew(totalJobs)
-		tick := time.Tick(time.Second)
-		for {
-			select {
-			case <-done:
-				withMutex(func() { bar.Set(totalJobs) })
-				bar.Update()
-				return nil
-			case <-tick:
-				withMutex(func() { bar.Set(jobsDone) })
-			}
-		}
+		return downloadFilenames(dest, source, filenames, maxDownloads)
 	},
 }
 
-type stdinIndex struct{ *bufio.Scanner }
+func downloadFilenames(dest, source FileSystem, filenames []string, maxDownloads int) error {
+	tokens := make(chan bool, maxDownloads)
+	for i := 0; i < maxDownloads; i++ {
+		tokens <- true
+	}
 
-func (i stdinIndex) Next() (*pga.Repository, error) {
-	if !i.Scan() {
-		if i.Err() == nil {
-			return nil, io.EOF
+	done := make(chan bool)
+	for _, filename := range filenames {
+		filename := filepath.Join("siva", "latest", filename[:2], filename)
+		go func() {
+			<-tokens
+			defer func() { tokens <- true }()
+
+			_, err := copy(dest, source, filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "could not get %s: %v\n", filename, err)
+			}
+			done <- true
+		}()
+	}
+
+	bar := pb.StartNew(len(filenames))
+	for i := 1; ; i++ {
+		<-done
+		bar.Set(i)
+		bar.Update()
+		if i == len(filenames) {
+			return nil
 		}
-		return nil, i.Err()
 	}
-	s := i.Text()
-	if !strings.HasSuffix(s, ".siva") {
-		return nil, fmt.Errorf("expected siva filename, got %s", s)
-	}
-	fs := strings.Split(s, ",")
-	return &pga.Repository{Filenames: fs}, nil
-}
-
-func getIndexFromStdin(r io.Reader) pga.Index {
-	return stdinIndex{bufio.NewScanner(r)}
-}
-
-func get(dest FileSystem, name string) error {
-	source := urlFS("http://pga.sourced.tech/")
-	ok, err := copy(dest, source, filepath.Join("siva", "latest", name[:2], name))
-	if !ok {
-		return err
-	} else if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	return nil
 }
 
 func init() {
