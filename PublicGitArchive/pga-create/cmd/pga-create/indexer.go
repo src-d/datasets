f04ccc94917fdb86797c072045a335e465a29a10
Manuel Carmona <manu.carmona90@gmail.com>
2019-04-09 17:46:39 +0100

    pga-create: improve files generated from the analyzed data
    Changes:
    
    - repack command: it only repacks watchers.csv and projects.csv from
      the ghtorrent's mysql dump.
    
    - discover command: it only generates one file named repositories.csv.gz
      containing all the information needed in the next steps
      (repository name, number of stars).
    
    - select command: it can only filter by number of stars. It generates
      two files: a list on the stdout of one url repository per line to
      feed borges and a file named repositories-index.csv.gz (filtered from
      repositories.csv.gz) to be used by the index generation command.
    
    - index command: it will use the repositories.csv.gz by default to give
      it the filtered information in repositories-index.csv.gz the flag -r
      must be used.
    
    Signed-off-by: Manuel Carmona <manu.carmona90@gmail.com>

--- PublicGitArchive/pga-create/cmd/pga-create/indexer.go
+++ PublicGitArchive/pga-create/cmd/pga-create/indexer.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
+	"compress/gzip"
+	"encoding/csv"
+	"io"
 	"os"
 	"runtime"
-	"strings"
+	"strconv"
 
-	"github.com/src-d/datasets/PublicGitArchive/pga-create"
+	indexer "github.com/src-d/datasets/PublicGitArchive/pga-create"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/src-d/core-retrieval.v0"
@@ -18,15 +20,13 @@ type indexCommand struct {
 	LogFile      string `long:"logfile" description:"write logs to file"`
 	Limit        uint64 `long:"limit" description:"max number of repositories to process"`
 	Offset       uint64 `long:"offset" description:"skip initial n repositories"`
-	Workers      int    `long:"workers" description:"number of workers to use (defaults to number of CPUs)"`
-	ReposFile    string `long:"repos-file" description:"path to a file with a repository per line, only those will be processed"`
-	Stars        string `short:"s" long:"stars" default:"data/stars.gz" description:"input path for the file with the numbers of stars per repository"`
-	Repositories string `short:"r" long:"repositories" default:"data/repositories.gz" description:"input path for the gzipped file with the repository names and identifiers"`
+	Workers      int    `long:"workers" description:"number of workers to use (defaults to GOMAXPROCS)"`
+	Repositories string `short:"r" long:"repositories" default:"data/repositories.csv.gz" description:"input path for the gzipped file with the repositories extracted information"`
 }
 
 func (c *indexCommand) Execute(args []string) error {
 	if c.Workers <= 0 {
-		c.Workers = runtime.NumCPU()
+		c.Workers = runtime.GOMAXPROCS(-1)
 	}
 
 	if c.Debug {
@@ -51,18 +51,9 @@ func (c *indexCommand) Execute(args []string) error {
 		logrus.SetOutput(f)
 	}
 
-	var repos []string
-	if c.ReposFile != "" {
-		content, err := ioutil.ReadFile(c.ReposFile)
-		if err != nil {
-			logrus.WithField("err", err).Fatalf("unable to read repositories file: %s", c.ReposFile)
-		}
-
-		for _, r := range strings.Split(string(content), "\n") {
-			if strings.TrimSpace(r) != "" {
-				repos = append(repos, r)
-			}
-		}
+	repos, err := loadReposList(c.Repositories)
+	if err != nil {
+		logrus.WithField("err", err).Fatal("unable to read " + c.Repositories)
 	}
 
 	indexer.Index(
@@ -73,9 +64,40 @@ func (c *indexCommand) Execute(args []string) error {
 		c.Limit,
 		c.Offset,
 		repos,
-		c.Repositories,
-		c.Stars,
 	)
 
 	return nil
+}
+
+func loadReposList(path string) (map[string]uint32, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	gzr, err := gzip.NewReader(f)
+	if err != nil {
+		return nil, err
+	}
+	defer gzr.Close()
+
+	r := csv.NewReader(gzr)
+	// read headers
+	if _, err := r.Read(); err != nil {
+		return nil, err
+	}
+
+	repos := map[string]uint32{}
+	for record, err := r.Read(); err != io.EOF; record, err = r.Read() {
+		stars, err := strconv.Atoi(record[1])
+		if err != nil {
+			return nil, err
+		}
+
+		repo := record[0]
+		repos[repo] = uint32(stars)
+	}
+
+	return repos, nil
 }
