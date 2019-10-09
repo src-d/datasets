5c827d427f01aa8922f1aa940368457973ae0c5c
Romain Keramitas <r.keramitas@gmail.com>
2019-10-09 10:08:20 +0200

    Refactor list.go
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- PublicGitArchive/pga/cmd/list.go
+++ PublicGitArchive/pga/cmd/list.go
@@ -5,7 +5,6 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -19,76 +18,58 @@ var listCmd = &cobra.Command{
 	Short: "list all the repositories in the index",
 	Long:  `List the repositories in the index, use flags to filter the results.`,
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		dataset, err := handleDatasetArg(cmd.Use, cmd.Flags())
+		if err != nil {
+			return err
+		}
 		ctx := setupContext()
-		f, err := getIndex(ctx)
+		f, err := getIndex(ctx, dataset.Name())
 		if err != nil {
 			return fmt.Errorf("could not open index file: %v", err)
 		}
-
-		index, err := pga.IndexFromCSV(f)
+		defer f.Close()
+		r := csv.NewReader(f)
+		err = dataset.ReadHeader(r)
 		if err != nil {
-			_ = f.Close()
 			return err
 		}
-
 		filter, err := filterFromFlags(cmd.Flags())
 		if err != nil {
-			_ = f.Close()
 			return err
 		}
-
 		formatter, err := formatterFromFlags(cmd.Flags())
 		if err != nil {
-			_ = f.Close()
 			return err
 		}
-
-		index = pga.WithFilter(index, filter)
-		for {
-			select {
-			case <-ctx.Done():
-				_ = f.Close()
-				return fmt.Errorf("command canceled")
-			default:
-			}
-
-			r, err := index.Next()
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				_ = f.Close()
-				return err
-			}
-
+		printRepository := func(r pga.Repository) error {
 			if s, err := formatter(r); err != nil {
-				fmt.Fprintf(os.Stderr, "could not format repository %s: %v\n", r.URL, err)
+				fmt.Fprintf(os.Stderr, "could not format repository %s: %v\n", r.GetURL(), err)
 			} else {
 				fmt.Print(s)
 			}
+			return nil
 		}
-
-		return f.Close()
+		return dataset.ForEach(ctx, r, filter, printRepository)
 	},
 }
 
-func formatterFromFlags(flags *pflag.FlagSet) (func(*pga.Repository) (string, error), error) {
+func formatterFromFlags(flags *pflag.FlagSet) (func(pga.Repository) (string, error), error) {
 	format, err := flags.GetString("format")
 	if err != nil {
 		return nil, err
 	}
-
 	switch format {
 	case "url":
-		return func(r *pga.Repository) (string, error) {
-			return r.URL + "\n", nil
+		return func(r pga.Repository) (string, error) {
+			return r.GetURL() + "\n", nil
 		}, nil
 	case "json":
-		return func(r *pga.Repository) (string, error) {
+		return func(r pga.Repository) (string, error) {
 			b, err := json.Marshal(r)
 			return string(b) + "\n", err
 		}, nil
 	case "csv":
-		return func(r *pga.Repository) (string, error) {
+		return func(r pga.Repository) (string, error) {
 			buf := new(bytes.Buffer)
 			w := csv.NewWriter(buf)
 			err := w.Write(r.ToCSV())
